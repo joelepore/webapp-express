e121f47adb00a7f8fb48f9976b8d2cea6fc3cff3
joelepore <giuseppejoelepore@gmail.com>
2025-02-06 13:35:35 +0100

    feat: implement show route with movie detail fetch and aggregated reviews

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -32,7 +32,26 @@ const index = (req, res) => {
 
 const show = (req, res) => {
   const id = req.params.id;
-  res.send(`Movie with id ${id} detail`);
+  const sql = `
+  SELECT movies.*, reviews.name AS review_name, reviews.vote, reviews.text
+  FROM movies
+  LEFT JOIN reviews ON movies.id = reviews.movie_id
+  WHERE movies.id = ?
+  `;
+
+  connection.query(sql, [id], (err, results) => {
+    if (err) return res.status(500).json({ error: err.message });
+    if (results.length === 0) return res.status(404).json({ error: 'Movie not found' });
+    // Pulisco l'oggetto movie
+    const { review_name, vote, text, ...movie } = results[0];
+
+    const aggregatedMovie = {
+      ...movie,
+      reviews: results.map(movie => ({ name: movie.review_name, vote: movie.vote, text: movie.text }))
+    }
+
+    res.json(aggregatedMovie);
+  })
 }
 
 export default {
